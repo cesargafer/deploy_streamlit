bc2f2d4a79c838f2b3df618ffc3f2d3dc3b466cb
cesargafer <cesargafer@gmail.com>
2024-02-23 10:24:22 +0000

    release version 1.0 add code for production, fixing bug during deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,16 +156,36 @@ def insertar_filme(nombre, director, genero, compañia):
 st.sidebar.title("Agregar Nuevo Filme")
 
 # Crear contenedores para los controles del formulario
-form_container = st.sidebar.beta_container()
+#form_container = st.sidebar.beta_container()
 
 # Controles para ingresar los datos del nuevo filme
-with form_container:
-    st.subheader("Ingrese los datos del nuevo filme")
-    nombre_input = st.text_input("Nombre del Filme")
-    director_input = st.text_input("Director")
-    genero_select = st.selectbox("Género", ["Acción", "Comedia", "Drama", "Ciencia Ficción", "Suspenso", "Animación", "Documental"])
-    compañia_input = st.text_input("Compañía Productora")
-    submit_button = st.form_submit_button("Insertar Filme")
+#with form_container:
+#    st.subheader("Ingrese los datos del nuevo filme")
+#    nombre_input = st.text_input("Nombre del Filme")
+#    director_input = st.text_input("Director")
+#    genero_select = st.selectbox("Género", ["Acción", "Comedia", "Drama", "Ciencia Ficción", "Suspenso", "Animación", "Documental"])
+#    compañia_input = st.text_input("Compañía Productora")
+#    submit_button = st.form_submit_button("Insertar Filme")
+
+
+#
+# Controles para ingresar los datos del nuevo filme en el sidebar
+st.sidebar.subheader("Ingrese los datos del nuevo filme")
+nombre_input = st.sidebar.text_input("Nombre del Filme")
+director_input = st.sidebar.text_input("Director")
+genero_select = st.sidebar.selectbox("Género", ["Acción", "Comedia", "Drama", "Ciencia Ficción", "Suspenso", "Animación", "Documental"])
+compañia_input = st.sidebar.text_input("Compañía Productora")
+submit_button = st.sidebar.button("Insertar Filme")
+
+# Botón para insertar el nuevo filme
+if submit_button:
+    # Validar que se hayan ingresado todos los datos
+    if nombre_input.strip() and director_input.strip() and genero_select.strip() and compañia_input.strip():
+        # Llamar a la función para insertar el nuevo filme en Firestore
+        insertar_filme(nombre_input, director_input, genero_select, compañia_input)
+    else:
+        st.warning("Por favor ingresa todos los datos del filme.")
+
 
 # Botón para insertar el nuevo filme
 if submit_button:
