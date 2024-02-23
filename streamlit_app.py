538ca8fada8551fd61cba056fa6643be6997d250
cesargafer <cesargafer@gmail.com>
2024-02-23 10:17:47 +0000

    release version 1.0 add code for production, fixing bug during deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ from google.oauth2 import service_account
 
 import json
 key_dict = json.loads(st.secrets["textkey"])
-creds = service_account.C
+#creds = service_account.C
+creds = service_account.Credentials.from_service_account_info(key_dict)
 
 
 from firebase_admin import initialize_app
